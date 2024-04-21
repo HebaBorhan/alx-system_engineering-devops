01465cd18c5f617c30b72a7f821fb5029f5a53fb
HebaZaki <h_borhan@hotmail.com>
2024-04-21 23:46:56 +0200

    SSH configuration file - Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # This script shows the SSH client configuration
 
-# Configure SSH client to use the private key ~/.ssh/school
+# using the private key ~/.ssh/school
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
 }
 
-# Configure SSH client to refuse password authentication
+# refusing password authentication
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
