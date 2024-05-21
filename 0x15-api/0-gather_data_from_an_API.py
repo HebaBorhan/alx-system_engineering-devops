80a4c44e46b63a5877863f5d38849b2594845ef5
HebaZaki <h_borhan@hotmail.com>
2024-05-21 12:52:38 +0300

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,43 @@
 #!/usr/bin/python3
 """using REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+returns information about his/her TODO list progress"""
+import json
 import re
-import requests
 import sys
+import urllib.request
 
+
+url = "https://jsonplaceholder.typicode.com"
 
 if __name__ == "__main__":
-    URL = "https://jsonplaceholder.typicode.com/"
-    EMPLOYEE_DATA = requests.get(URL + "users/{}".format(sys.argv[1])).json()
-    EMPLOYEE_NAME = EMPLOYEE_DATA.get("name")
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            employee_id = int(sys.argv[1])
 
-    Tasks_4_emply = requests.get(
-        URL + "todos/", params={"userId": sys.argv[1]}).json()
-    COMPLETED_TASKS = []
-    for TASK_TITLE in Tasks_4_emply:
-        if TASK_TITLE.get("completed"):
-            COMPLETED_TASKS.append(TASK_TITLE.get("title"))
-    len_T_C = (len(COMPLETED_TASKS))
-    len_e_t_c = (len(Tasks_4_emply))
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, len_T_C, len_e_t_c))
-    for task in COMPLETED_TASKS:
-        print("\t {}".format(TASK_TITLE))
+            # Fetch employee details
+            with urllib.request.urlopen(
+                    f"{url}/users/{employee_id}") as response:
+                request = json.loads(response.read().decode())
+
+            # Fetch employee's todos
+            with urllib.request.urlopen(f"{url}/todos") as response:
+                todo_req = json.loads(response.read().decode())
+
+            # Extract employee name
+            EMPLOYEE_NAME = request.get('name')
+
+            # Filter tasks by employee ID
+            TOTAL_NUMBER_OF_TASKS = list(filter(
+                lambda x: x.get('userId') == employee_id, todo_req))
+
+            # Filter done tasks
+            NUMBER_OF_DONE_TASKS = list(
+                filter(lambda x: x.get('completed'), TOTAL_NUMBER_OF_TASKS))
+
+            # Print the result
+            print(
+                "Employee {} is done with tasks({}/{}):".format(
+                    EMPLOYEE_NAME, len(
+                        NUMBER_OF_DONE_TASKS), len(TOTAL_NUMBER_OF_TASKS)))
+            for task in NUMBER_OF_DONE_TASKS:
+                print('\t {}'.format(task.get('title')))
