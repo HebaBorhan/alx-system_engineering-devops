cbb936d2eb5911136d0c0254f70b56c42a7b50e4
HebaZaki <h_borhan@hotmail.com>
2024-05-21 13:16:05 +0300

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,7 +9,7 @@ url = "https://jsonplaceholder.typicode.com"
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        employee_id = int(sys.argv[1])
+        EMPLOYEE_ID = int(sys.argv[1])
 
         # Fetch employee details
         employee_response = requests.get(f"{url}/users/{employee_id}")
@@ -20,15 +20,14 @@ if __name__ == "__main__":
 
         # Fetch user's TODO list
         tasks_response = requests.get(
-            f"{url}/todos", params={"userId": employee_id})
+            f"{url}/todos", params={"userId": EMPLOYEE_ID})
         tasks = tasks_response.json()
 
         # Write to CSV file
-        with open(f"{employee_id}.csv", 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        file = f"{EMPLOYEE_ID}.csv"
+        with open(file, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
             for task in tasks:
                 writer.writerow(
-                    [employee_id, EMPLOYEE_NAME, task.get(
+                    [EMPLOYEE_ID, EMPLOYEE_NAME, task.get(
                         'completed'), task.get('title')])
