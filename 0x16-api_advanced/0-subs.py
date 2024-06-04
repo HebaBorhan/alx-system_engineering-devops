c25511e94b2146e268a359fe0f9c2ca785752965
HebaZaki <h_borhan@hotmail.com>
2024-06-04 23:12:10 +0300

    reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,10 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; MyRedditApp/0.1)'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
