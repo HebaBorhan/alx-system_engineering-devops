70f13f1112e02c88157242fad7add6a0a9f9b9cd
HebaZaki <h_borhan@hotmail.com>
2024-06-04 23:37:44 +0300

    recursion

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -52,5 +52,3 @@ def count_words(subreddit, word_list, after=None, word_count=None):
             return
     except requests.RequestException:
         return
-
-count_words("python", ["Python", "javascript", "java", "C++", "Go"])
