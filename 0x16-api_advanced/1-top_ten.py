a949d1d4bc4e145c162f58134e3b8299411555cd
HebaZaki <h_borhan@hotmail.com>
2024-06-04 23:19:22 +0300

    reddit 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""This script queries the Reddit API
+and prints the titles of the first 10 hot posts listed for a given subreddit"""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    """returns titles of first 10 hot posts listed for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MyRedditApp/0.1)'}
+
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
