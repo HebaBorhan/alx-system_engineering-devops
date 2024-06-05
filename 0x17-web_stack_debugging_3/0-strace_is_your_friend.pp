9fe23159e471b789c81233e48980e6cf350d3473
HebaZaki <h_borhan@hotmail.com>
2024-06-05 14:30:24 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,4 @@
-# Path to the wp-settings.php file
-$file_path = '/var/www/html/wp-settings.php'
-
-file { $file_path:
-  ensure => file,
-}
-
-# Correcting typo in wp-settings.php file
-exec { 'fix_php_typo':
-  command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" $file_path',
-  require => File[$file_path],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
