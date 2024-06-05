a3702f75b5320bea05140ad3db122c15051ede34
HebaZaki <h_borhan@hotmail.com>
2024-06-05 14:59:52 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -10,7 +10,16 @@ exec { 'install_puppet_lint':
   require => Package['ruby'],
 }
 
-exec { 'fix-typo_php':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Define the file path
+$file_path = '/var/www/html/wp-settings.php'
+
+# Ensure the file exists
+file { $file_path:
+  ensure => file,
+}
+
+# Correct the typo in the wp-settings.php file
+exec { 'fix_php_typo':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' ${file_path}",
+  require => File[$file_path],
 }
