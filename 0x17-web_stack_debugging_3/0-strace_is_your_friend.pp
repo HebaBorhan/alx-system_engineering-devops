73c8f1f67d90ba468dc7e7b426bb6503c0e5949c
HebaZaki <h_borhan@hotmail.com>
2024-06-05 14:35:05 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,15 @@
+# Install Ruby
+package { 'ruby':
+  ensure => installed,
+}
+
+# Install puppet-lint gem
+exec { 'install_puppet_lint':
+  command => 'gem install puppet-lint -v 2.1.1',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  require => Package['ruby'],
+}
+
 # Path to the wp-settings.php file
 $file_path = '/var/www/html/wp-settings.php'
 
