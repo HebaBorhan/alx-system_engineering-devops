519c02861f696a1abc4361d625afeabafa550bd4
HebaZaki <h_borhan@hotmail.com>
2024-06-13 11:29:20 +0300

    update ulimit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This script set a new value for ULIMIT
 
 exec { 'update_ulimit_value':
-  command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
+  command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx  && sudo service nginx restart',
   path    => '/bin:/usr/bin',
 }
